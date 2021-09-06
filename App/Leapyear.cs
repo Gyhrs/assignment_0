56fcdbef2f49b5bfd0af2f3b3efe2cfd63887c37
Tue Edmund Gyhrs <tugy@itu.dk>
2021-09-06 15:15:31 +0200

    Added Exceptions for input

--- App/Leapyear.cs
+++ App/Leapyear.cs
@@ -4,18 +4,37 @@ namespace App
 {
     public class Leapyear
     {
-        public Leapyear() {}
-        public Leapyear(int inputtedyear) {
-            if (isLeapYear(inputtedyear))
+        public void promptUser()
+        {
+            Console.WriteLine("Type in a year to learn if it is a leapyear!");
+            var input = Console.ReadLine();
+            promptUser(input);
+        }
+        public void promptUser(String input)
+        {
+            try
             {
-                Console.WriteLine("yay");
-            } else
+                int inputasint = Convert.ToInt32(input);
+                if (isLeapYear(inputasint))
+                {
+                    Console.WriteLine("yay");
+                } else 
+                {
+                    Console.WriteLine("nay");
+                }
+            }
+            catch (ArgumentException ae)
             {
-                Console.WriteLine("nay");
+                Console.WriteLine(ae.Message);
+                promptUser();
             }
         }
         public bool isLeapYear(int year) 
         {
+            if (year < 1582)
+            {
+                throw new ArgumentException("Year must be 1582 or higher");
+            }
             if (year % 4 == 0)
             {
                 if (year % 100 == 0)
